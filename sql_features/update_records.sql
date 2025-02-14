63c91e3514886556c7a084e3a90034bdf4e28cad
Alissa Beaderstadt <alissabeaderstadt@outlook.com>
2025-02-13 20:42:15 -0600

    Add additional columm.

--- sql_features/update_records.sql
+++ sql_features/update_records.sql
@@ -9,3 +9,16 @@ WHERE title = 'Twilight';
 UPDATE books
 SET retail_price = 9.99
 WHERE title = 'Harry Potter and the Sorcerer’s Stone';
+
+-- Add page count collumn
+ALTER TABLE books
+ADD COLUMN page_count INTEGER;
+
+-- Set the page count for specific books
+UPDATE books
+SET page_count = 498
+WHERE title = 'Twilight';
+
+UPDATE books
+SET page_count = 309
+WHERE title = 'Harry Potter and the Sorcerer’s Stone';
